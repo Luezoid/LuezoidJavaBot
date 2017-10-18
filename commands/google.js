56b6cdc80ca6d38f76fe5b2b0529861ec09c7eaf
Luezoid <32596294+Luezoid@users.noreply.github.com>
2017-10-18 18:33:24 +0100

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client
 const fs = require('fs')
 client.commands = new Discord.Collection()
-const google = require('google');
+const google = require('google')
 
 module.exports.run = async (client, message, args) => {
 	const toSearch = args.join(' ');
