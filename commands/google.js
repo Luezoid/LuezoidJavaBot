3ba181fe9e2b561edefed3f097a8faeae102f905
Luezoid <32596294+Luezoid@users.noreply.github.com>
2017-10-18 18:43:36 +0100

    Add files via upload

---
+++ commands/google.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client
+const fs = require('fs')
+client.commands = new Discord.Collection()
+const google = require('google')
+
+module.exports.run = async (client, message, args) => {
+	const toSearch = args.join(' ');
+	let links = [];
+	if (!toSearch) {
+			return message.reply('Please insert something to search');
+	}
+
+	google(toSearch, async (err, res) => {
+			for (let i = 0; i < 3; i++) {
+					if (res.links[i].href !== null) {
+							links.push('<' + res.links[i].href + '>');
+					}
+			}
+			await message.react('✅');
+			await message.channel.send(`**I found:**\n${links.join('\n')}`);
+	});
+};
+
+module.exports.help = {
+    name: "google"
+}
